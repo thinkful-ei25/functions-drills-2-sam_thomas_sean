8ba3ffa206ab7e59381cd6fcd8dd4d55b0da5878
Thomas Chun <tchun2055@gmail.com>
2018-09-24 14:03:46 -0700

    days in a month with leap years included

--- jedi-name.js
+++ jedi-name.js
@@ -59,3 +59,36 @@ function decode(word){
 
 let decode1  = decode('craft block argon meter bells brown croon droop');
 console.log(decode1);
+
+
+function daysInAMonth(month, leapYear = false) {
+    switch(month) {
+        case 'January':
+        case 'March':
+        case 'May':
+        case 'July':
+        case 'August':
+        case 'October':
+        case 'December':
+            return `${month} has 31 days`;
+            break;
+        case 'April':
+        case 'June':
+        case 'September':
+        case 'November':
+            return `${month} has 30 days`;
+            break;
+        case 'February':
+            if (leapYear === true) {
+                return `${month} has 29 days`;
+            } else {
+                return `${month} has 28 days`;
+            }
+            break;
+    }
+}
+
+let days = daysInAMonth('February', true);
+console.log(days);
+
+
