e0659e984a24d3bbbf79b86faa2a17c2b84ebc98
Sean Nealon <signalflowsean@gmail.com>
2018-09-24 16:24:32 -0400

    added beyond function

--- jedi-name.js
+++ jedi-name.js
@@ -1,6 +1,30 @@
+'use strict'; 
 function jediName(firstName, lastName) {
-    return lastName.slice(0, 3) + firstName.slice(0, 2);
+  return lastName.slice(0, 3) + firstName.slice(0, 2);
 }
 
 let jedi = jediName('Beyonce', 'Knowles');
 console.log(jedi);
+
+function beyond(num){
+    if (num === Number.POSITIVE_INFINITY || num === Number.NEGATIVE_INFINITY){ 
+        console.log('And beyond'); 
+    }
+    else if (num === Number.isFinite && num> 0){ 
+        console.log('To Infinity'); 
+    }
+    else if (num === Number.isFinite && num < 0){ 
+        console.log('To negative Infinity'); 
+    }
+    else if (num === 0){ 
+        console.log('Staying home'); 
+    }    
+    else { 
+        console.log('oops!'); 
+    }
+
+}
+
+beyond(Number.POSITIVE_INFINITY); 
+
+//console.log(checkBeyond); 
